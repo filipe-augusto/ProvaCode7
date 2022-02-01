4b292637f0997fa2ae3bb3d29205d25e74f6153e
Filipe Augusto Santos Chaves <67299107+filipe-augusto@users.noreply.github.com>
2022-02-01 20:25:27 -0300

    esquema de criar

--- Shared/ViewModels/ClienteViewModel.cs
+++ Shared/ViewModels/ClienteViewModel.cs
@@ -5,11 +5,16 @@ namespace ProvaCode7.Shared
     public class ClienteViewModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo necessario")]
+        [MinLength(5,ErrorMessage ="Minimo 5 letras")]
         public string Nome { get; set; }
         [Required(ErrorMessage ="Campo necessario")]
+        [MaxLength(10, ErrorMessage ="Maximo 10")]
         public string Cpf { get; set; }
 
         [Required(ErrorMessage = "Campo necessario")]
+        [MaxLength(15, ErrorMessage = "Maximo 15")]
         public string Telefone { get; set; }
 
         public decimal Credito { get; set; }
--- Shared/ViewModels/EnderecoViewModel.cs
+++ Shared/ViewModels/EnderecoViewModel.cs
@@ -8,15 +8,23 @@ namespace ProvaCode7.Shared
     public class EnderecoViewModel
     {
         [Required(ErrorMessage = "Campo necessario")]
+        [MinLength(10, ErrorMessage = "Minimo 10 letras")]
         public string Cep { get; set; }
+        [MinLength(50, ErrorMessage = "Minimo 50 letras")]
         [Required(ErrorMessage = "Campo necessario")]
         public string Logradouro { get; set; }
+   
         [Required(ErrorMessage = "Campo necessario")]
+        [MinLength(50, ErrorMessage = "Minimo 50 letras")]
         public string Bairro { get; set; }
         [Required(ErrorMessage = "Campo necessario")]
+        [MinLength(50, ErrorMessage = "Minimo 50 letras")]
         public string Cidade { get; set; }
         [Required(ErrorMessage = "Campo necessario")]
         public string Estado { get; set; }
+
+        public string Numero { get; set; }
+        [MinLength(50, ErrorMessage = "Minimo 50 letras")]
         public string Complemento { get; set; }
 
     }
